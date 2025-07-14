8cb198f24dfb751d3e0b9d6d26765f0b65a638c1
ron131977 <ronalddsouza888@gmail.com>
2025-07-14 22:16:34 +0530

    finally go

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="google-adsense-account" content="ca-pub-9514296812201677" />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9514296812201677"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  )
+}
