f17f5665e817ceff2b23b9b68b372b3f4ba6870c
ron131977 <ronalddsouza888@gmail.com>
2025-07-14 22:37:23 +0530

    FUCK TO YOU

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+     <meta name="robots" content="index, follow" />
+      <meta name="google-adsense-account" content="ca-pub-9514296812201677" />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9514296812201677"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  );
+}
